04d13d2df80168c439a5ba9dd356b7b9f4d7fcfd
Eric Blue <eblue904@gmail.com>
2020-07-27 02:10:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,18 @@
-function basicTeenager(age) {
+function basicTeenager(age){
+  if(age >= 13 && age <= 19)
+  return "You are a teenager!";
+
 
 }
 
 function teenager(age) {
+  if(age >= 13 && age <= 19)
+  return "You are a teenager!";
+//} else (age <= 13 && age >= 19)
+//  return "You are not a teenager"
+
+//}
+
 
 }
 
